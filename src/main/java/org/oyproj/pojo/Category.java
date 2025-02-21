bff492277a1194644e4aa51bc4de077809852fc5
oywq2020 <2403_87019412@noreply.gitcode.com>
2025-02-21 18:36:30 +0800

    optimize category update; use validation classification fix the interrupt of validation, detail in Category class

--- src/main/java/org/oyproj/pojo/Category.java
+++ src/main/java/org/oyproj/pojo/Category.java
@@ -3,17 +3,19 @@ package org.oyproj.pojo;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.groups.Default;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class Category {
-    @NotNull
+    @NotNull(groups = Update.class)
     private Integer id;//主键ID
-    @NotEmpty
+    @NotEmpty //default group
     private String categoryName;//分类名称
-    @NotEmpty
+    @NotEmpty //default group
     private String categoryAlias;//分类别名
     private Integer createUser;//创建人ID
     //assign format when database convert to object
@@ -21,4 +23,18 @@ public class Category {
     private LocalDateTime createTime;//创建时间
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;//更新时间
+
+
+
+
+    //validation classification
+    //if validation group is not assigned,it follows the default
+    //group also can be extended
+    public interface Add extends Default {
+
+    }
+
+    public interface Update extends Default{
+
+    }
 }
